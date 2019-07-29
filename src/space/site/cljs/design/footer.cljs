e976c9485cdb1c70fa0ac7077481db3063451920
Ashley Smith <ashley@aas.sh>
2019-07-29 15:45:26 +0100

    Split the design namespace into navbar and footer - This will help with keeping things tidy - Whether the core namespace is necessary is uncertain - If it is kept, common calls like getting the site name can be done easily - If it is removed, it'd be easy for other files to just require the thing they need

---
+++ src/space/site/cljs/design/footer.cljs
@@ -0,0 +1,53 @@
+(ns space.site.cljs.design.footer)
+
+(defn footer
+  "Footer of website linking to repository"
+  []
+  [:footer.footer
+    [:div.container.is-fluid
+      [:div.container
+          {:style {:max-width "700px"}}
+        [:div.content
+
+          ;; Logo
+          [:h1.title 
+            [:span.icon
+              [:i.fas.fa-rocket]]
+            " Space"]]
+        [:div.columns
+          [:div.column
+
+            ;; Space information and GitHub link
+            [:div.content
+              [:p "Space is an open-source forum with a modern take on discussions
+                  and collaboration designed from the ground up to be forked and
+                  customised to suit your community's needs."]]
+            [:a.button.is-primary
+                {:href "https://github.com/ashe/space"}
+              [:span.icon
+                [:i.fab.fa-github]]
+              [:span "Fork on GitHub"]]]
+          [:div.column
+
+            ;; Forum links
+            ;; @TODO: Link these to somewhere
+            [:div.content
+              [:div.content.is-marginless 
+                [:a "Dashboard"]]
+              [:div.content.is-marginless 
+                [:a "What is Space?"]]
+              [:div.content.is-marginless 
+                [:a "Why open source?"]]]
+
+            ;; Link to author
+            [:div.content.is-marginless
+              [:strong "Made by "
+                [:a 
+                  {:href "https://aas.sh"}
+                  "Ashley Smith"]
+                " with "
+                [:a 
+                  {:href "https://clojure.org"}
+                  "Clojure"]
+                " and "
+                [:span.icon [:icon.fa.fa-heart]]]]]]]]])
---
+++ src/space/site/cljs/design/navbar.cljs
@@ -0,0 +1,48 @@
+(ns space.site.cljs.design.navbar
+  (:require [space.site.cljs.events.time :as time]))
+
+;; @TODO: Change appearance depending on current page
+(defn navbar
+  "Navbar of site"
+  []
+  [:div
+    [:section.hero.is-primary.is-small
+
+      ;; Top bar
+      [:div.hero-head
+        [:nav.navbar
+          [:div.container
+            [:div.navbar-brand
+              [:span.navbar-item
+                [:a.button.is-primary.is-large
+                    {:href "/"}
+                  [:h1.title 
+                    [:span.icon
+                      [:i.fas.fa-rocket]]
+                    " Space"]]]
+              [:span.navbar-burger.burger
+                  {:data-target "navbarMenuHeroA"}
+                [:span]
+                [:span]
+                [:span]]]
+            [:div.navbar-menu
+                {:id "navbarMenuHeroA"}
+              [:div.navbar-end
+                [:span.navbar-item
+                  (time/get-current-time)]
+                [:span.navbar-item
+                  [:a.button.is-primary.is-inverted
+                    [:span.icon
+                      [:i.fa.fa-user]]
+                    [:span "Sign in"]]]]]]]
+        
+      ;; Hero footer
+      [:div.hero-foot
+        [:nav.tabs
+          [:div.container
+            [:ul
+              [:li.is-active [:a {:href "/"} "Forum"]]
+              [:li [:a {:href "/tags"} "Tags"]]
+              [:li [:a {:href "/members"} "Members"]]
+              [:li [:a {:href "/admin"} "Admin"]]]]]]]]])
+
