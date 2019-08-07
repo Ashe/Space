d51ebc4cf74cb05aeecc8001d9140970559f0132
Ashley Smith <ashley@aas.sh>
2019-08-07 19:24:13 +0100

    Made input validation generic and work on content bos

--- src/space/site/cljs/views/create/post.cljs
+++ src/space/site/cljs/views/create/post.cljs
@@ -1,7 +1,7 @@
 (ns space.site.cljs.views.create.post
   (:require [reagent.core :as r]))
 
-(declare form info-panel)
+(declare form info-panel make-text-input)
 
 (defn create-post
   "Shows an overview of post expectations and the form"
@@ -51,14 +51,18 @@
 ;; Constraints for post
 (def title-min 10)
 (def title-max 100)
+(def content-min 25)
+(def content-max 0)
 
 ;; Atoms for form validation
 (def title (r/atom ""))
+(def content (r/atom ""))
 (def has-agreed (r/atom false))
 (defn ready-to-submit [] (and
     @has-agreed 
-    (> (count @title) title-min)
-    (< (count @title) title-max)))
+    (>= (count @title) title-min)
+    (<= (count @title) title-max)
+    (>= (count @content) content-min)))
 
 (defn form
   "Form for creating a new post"
@@ -66,31 +70,10 @@
   [:div
 
     ;; Title
-    (let [length (count (or @title ""))
-          colour (cond
-                    (zero? length) ""
-                    (or (< length title-min) (> length title-max)) "is-danger"
-                    :else "is-success")
-          icon (if (and (>= length title-min) (<= length title-max))
-                    "fa-check" "fa-exclamation-triangle")]
-      [:div.field
-        [:label.label "Post title"]
-        [:div.control.has-icons-right
-          [:input.input
-            { :type "text"
-              :placeholder "I've been thinking about.."
-              :on-change #(reset! title (.-value (.-target %)))
-              :class colour}]
-          [:span.icon.is-small.is-right
-            [:i.fas {:class icon}]]]
-          (cond
-            (< length title-min) [:p.help
-                {:class colour}
-              "Please enter a valid title (" (str (- title-min length)) " characters left)"]
-            (< length title-max) [:p.help.is-success
-              "Thanks! " (str (- title-max length)) " characters left"]
-            :else [:p.help.is-danger
-              "Please enter a valid title (" (str (- length title-max)) " characters too many)"])])
+    (make-text-input title 
+        :input.input 
+        "Title" "Thanks!" "Please enter a descriptive title" 
+        title-min title-max)
 
     ;; Tags
     ;; @TODO: Implement the ability to grab tags
@@ -106,16 +89,10 @@
         "Please provide at least one tag to give context to your post"]]
 
     ;; Body
-    [:div.field
-      [:label.label "Content"]
-      [:div.control.has-icons-right
-        [:textarea.textarea.is-danger
-          {:type "text"
-           :placeholder "What have you been thinking about?"}]
-        [:span.icon.is-small.is-right
-          [:i.fas.fa-exclamation-triangle]]]
-      [:p.help.is-danger
-        "Please write a post with at least 20 characters"]]
+    (make-text-input content 
+        :textarea.textarea 
+        "Content" "Thanks!" "Please write your post" 
+        content-min content-max)
 
     ;; Agree to forum rules
     [:div.field
@@ -141,3 +118,40 @@
               {:href "/"}
             "Back to forum"]]]
     ])
+
+(defn make-text-input
+  "Makes an input with a given atom, tag and set of messages"
+  [input-atom input-type label help-okay help-invalid min-length max-length]
+  (let [length (count (or @input-atom ""))
+        colour (cond
+                  (zero? length) ""
+                  (or 
+                    (and (> min-length 0) (< length min-length))
+                    (and (> max-length 0) (> length max-length))) 
+                    "is-danger"
+                  :else "is-success")
+        icon (if (and 
+                   (or (<= min-length 0) (>= length min-length)) 
+                   (or (<= max-length 0) (<= length max-length)))
+                "fa-check" "fa-exclamation-triangle")]
+  [:div.field
+    [:label.label label]
+    [:div.control.has-icons-right
+      [input-type
+        { :type "text"
+          :placeholder "I've been thinking about.."
+          :on-change #(reset! input-atom (.-value (.-target %)))
+          :class colour}]
+      [:span.icon.is-small.is-right
+        [:i.fas {:class icon}]]]
+      (cond
+        (< length min-length) 
+          [:p.help
+              {:class colour}
+            help-invalid " (" (str (- min-length length)) " characters to go)"]
+        (or (<= max-length 0) (< length max-length))
+          [:p.help.is-success
+            help-okay (when (pos? max-length) (str " (" (str (- max-length length)) " characters left)"))]
+        (> max-length 0)
+          [:p.help.is-danger
+            help-invalid " (" (str (- length max-length)) " characters too many)"])]))
