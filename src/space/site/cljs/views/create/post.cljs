f968f7eab32a9047c4a9e373ab85ac3b298bd47c
Ashley Smith <ashley@aas.sh>
2019-08-19 15:14:55 +0100

    Simple MDE integrated, needs some form labels and hints like before

--- src/space/site/cljs/views/create/post.cljs
+++ src/space/site/cljs/views/create/post.cljs
@@ -5,7 +5,7 @@
             [space.site.cljs.events.post :as p]))
 
 ;; Forward declare
-(declare form info-panel)
+(declare form info-panel md-editor)
 
 ;; Constraints for post
 (def title-min 10)
@@ -124,12 +124,14 @@
       [:p.help.is-danger
         "Please provide at least one tag to give context to your post"]]
 
+    [md-editor content {}]
+
     ;; Body
-    (f/make-text-input content 
-        :textarea.textarea "text"
-        "Content*" "What do you want to talk about?" "fa-pencil"
-        "Thanks!" "Please write your post" 
-        content-min content-max)
+    ;(f/make-text-input content 
+    ;   [md-editor content] "text"
+    ;   "Content*" "What do you want to talk about?" "fa-pencil"
+    ;   "Thanks!" "Please write your post" 
+    ;   content-min content-max)
 
     ;; Ask the user if they want their post to remain anonymous
     (when @(rf/subscribe [:user])
@@ -153,4 +155,15 @@
               {:href "/"}
             "Back to forum"]]]])
 
-
+(defn- md-editor 
+  [input-atom attrs]
+  (r/create-class
+    { :component-did-mount
+        (fn [comp]
+          (let [mde (js/SimpleMDE. (clj->js {:element (r/dom-node comp)}))]
+            (.on mde.codemirror "change" 
+                #(reset! input-atom (.value mde)))
+            mde))
+      :reagent-render
+        (fn []
+          [:textarea.textarea attrs])}))
