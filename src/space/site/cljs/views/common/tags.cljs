83fdfcce1eea13f7fc315499f79fee54a1db4b0d
Ashley Smith <ashley@aas.sh>
2019-09-01 17:09:24 +0100

    Added button for post-praising, doesn't do anything yet

--- src/space/site/cljs/views/common/tags.cljs
+++ src/space/site/cljs/views/common/tags.cljs
@@ -1,11 +1,14 @@
 (ns space.site.cljs.views.common.tags
   (:require [clojure.string :as str]
-            [re-frame.core :as rf]))
+            [reagent.core :as r]
+            [re-frame.core :as rf]
+            [space.site.cljs.events.notifications :as n]))
 
 (defn make-tag
   "Make a tag from a tag's ID"
   [tag-data]
   (let [info @(rf/subscribe [:space-info])
+        user @(rf/subscribe [:user])
         tag 
             (merge 
               tag-data 
@@ -13,15 +16,26 @@
     (when tag
       [:div.control 
           {:key (str "tag-" (:id tag))}
-        [:a 
-            { :href (str "/tags/" (:label tag))
-              :style {:text-decoration "none"}}
-          [:div.tags.has-addons
-            [:span.tag.is-primary
-                {:style
+        [:div.tags.has-addons
+          [:span.tag.is-primary
+            [:a 
+                { :href (str "/tags/" (:label tag))
+                  :style 
                   { :color "#ffffff"
                     :background-color "#00d1b2"}}
-              (str/capitalize (:label tag))]
-            (when (contains? tag :points)
-              [:span.tag.is-dark 
-                (str (:points tag))])]]])))
+              (str/capitalize (:label tag))]]
+          (when (contains? tag :points)
+            (seq [[:span.tag.is-dark 
+              (str (:points tag))]
+            (when user
+              [:span.tag.is-primary
+                [:a 
+                    { :style {:color "#ffffff"}
+                      :on-click
+                        (n/dispatch-notification
+                            "Cannot praise post"
+                            "Feature not yet implemented"
+                            "is-danger"
+                            "fa-do-not-enter")}
+                  [:i.fas.fa-chevron-up]]])]))
+               ]])))
